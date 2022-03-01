4b4da3ec1b5ab2784921dd1f3a4d8c16d38c0e5c
toshiya <arche.dsp@gmail.com>
2022-03-01 11:12:39 -0600

    Editando para añadir emailbackend

--- Myweb/settings-prod.py
+++ Myweb/settings-prod.py
@@ -131,10 +131,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 # Email Settings
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('email_user')
 EMAIL_HOST_PASSWORD = os.getenv('email_pass')
 
