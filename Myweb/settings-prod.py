28551b4502319053ccfd038a1c640392f56315f7
toshiya <arche.dsp@gmail.com>
2022-02-28 23:21:30 -0600

    Editando settings de prod con host permitido

--- Myweb/settings-prod.py
+++ Myweb/settings-prod.py
@@ -27,9 +27,9 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["smabell.pythonanywhere.com"]
 
 
 # Application definition
