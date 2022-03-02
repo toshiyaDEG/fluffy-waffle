358ec759ccfebc9cc1145cddae3d2a6f006a7be6
toshiya <arche.dsp@gmail.com>
2022-03-01 20:59:22 -0600

    Editando .env para usuario y pass

--- Myweb/settings-prod.py
+++ Myweb/settings-prod.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env('email_user')
+EMAIL_HOST_USER = os.getenv('email_user')
 EMAIL_HOST_PASSWORD = os.getenv('email_pass')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
